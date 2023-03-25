83cf82ba10c5b3dca472fa07dd04cc3e397a6d3a
Alex <56222820+AlexMorgul@users.noreply.github.com>
2023-03-25 14:33:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,36 +2,15 @@ const express = require('express');
 const request = require('request');
 const app = express();
 
-app.use(express.json());
-
 var port = '3000';
+
+app.use(express.json());
 
 app.post('/', async (req, res) => {
 
-  console.log('Метод отрабатывает. Body:\n' + req.body);
-  res.send('Все кайф!');
-  
-  
-//   if (!req.body) return res.sendStatus(400);
-
-//   let data = req.body;
-
-//   if (data.isExtend != null) {
-
-//   } else {
-//     await request.post('https://api.monobank.ua/api/merchant/invoice/create',
-//       {
-//         headers: {
-//           'Content-Type': 'application/json',
-//           'X-Token': 'ujaMDwOnUtpOK-ViXhqBCczdbua3pAkawIch6tgheZ4U'
-//         },
-//         body: JSON.stringify(data)
-//       },
-//       (error, response, body) => {
-//         console.log(body);
-//       }
-//     );
-//   }
+  let data = req.body;
+  console.log('data: ' + req.body.text)
+  res.send('fuck u')
 });
 
 
